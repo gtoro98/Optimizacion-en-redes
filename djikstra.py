bfa56f127fd06ad383ee31b57ffc05a127442006
Carlos Freitas <47233287+CarlosSoki@users.noreply.github.com>
2021-11-21 02:15:52 -0400

    Update djikstra.py

--- djikstra.py
+++ djikstra.py
@@ -2,12 +2,13 @@ matriz_distancias = []
 
 def camino_mas_corto(matriz_ad, nodoInicio):
     inicio = nodoInicio
-
+    matriz_distancias.clear()
+    
     for i in range(len(matriz_ad)): #0-35 preparar estructura de datos auxiliar: matriz nodo distancia minima predecesor y visitado
         if i == inicio:
-            matriz_distancias.append({"nodo": i, "min_distancia": 0, "predecesor": None, "visitado": False})
+            matriz_distancias.append({"nodo": i, "min_distancia": 0, "predecesor": None, "predecesor_alt": None,"visitado": False})
         else:
-             matriz_distancias.append({"nodo": i, "min_distancia": 999, "predecesor": None, "visitado": False})
+             matriz_distancias.append({"nodo": i, "min_distancia": 999, "predecesor": None, "predecesor_alt": None, "visitado": False})
                 
     #Imprimir la matriz auxiliar
     print("\nINICIALIZACIÓN MATRIZ AUXILIAR:")
@@ -47,22 +48,76 @@ def visitar_nodo(nodo, matriz_ad):
                     matriz_distancias[revisando['nodo']]['min_distancia'] = matriz_ad[nodo['nodo']][x] + nodo['min_distancia'] # cambia 999 por 5
                     matriz_distancias[revisando['nodo']]['predecesor'] = nodo['nodo']
                     print("-Nodo:"+str(revisando["nodo"])+" "+ str(matriz_distancias[revisando['nodo']])+"\n")
+
+                elif matriz_ad[nodo['nodo']][x] + nodo['min_distancia'] == revisando['min_distancia']: #5 + 0 < 999
+                    print("-El nuevo camino al nodo ("+str(revisando['nodo'])+") es "+str(matriz_ad[nodo['nodo']][x] + nodo['min_distancia'])+", es igual que el viejo "+str(revisando['min_distancia']))
+                    matriz_distancias[revisando['nodo']]['predecesor_alt'] = nodo['nodo']
+                    print("-Nodo:"+str(revisando["nodo"])+" "+ str(matriz_distancias[revisando['nodo']])+"\n")
                 else:
-                    print("-El nuevo camino al nodo ("+str(revisando['nodo'])+") es "+str(matriz_ad[nodo['nodo']][x] + nodo['min_distancia'])+", es más largo o igual que el viejo "+str(revisando['min_distancia']))
+                    print("-El nuevo camino al nodo ("+str(revisando['nodo'])+") es "+str(matriz_ad[nodo['nodo']][x] + nodo['min_distancia'])+", es más largo que el viejo "+str(revisando['min_distancia']))
                     print("-NO se actualiza el nodo ("+str(revisando["nodo"])+")\n")
                     
     return
 
-def recontruir_camino(matriz_distancias, nodoInicio, nodoDestino):
+def reconstruir_camino(matriz_distancias, nodoInicio):
     Inicio = nodoInicio
-    Destino = nodoDestino
-    Cadena = Destino
-    Este_nodo = Destino
+    DestinoJavier = 28
+    DestinoAndreina = 15
+    Cadena_Javier = []
+    Cadena_Andreina = []
+
+    Cadena_Javier.append(DestinoJavier)
+    Cadena_Andreina.append(DestinoAndreina)
+    Este_nodo = DestinoJavier
     
     while (Este_nodo != Inicio):
         Este_nodo = matriz_distancias[Este_nodo]['predecesor']
-        Cadena = str(Este_nodo) +" -> "+str(Cadena)
+        Cadena_Javier.append(Este_nodo)
+
+    Este_nodo = DestinoAndreina
+    distancia_Andreina = matriz_distancias[DestinoAndreina]['min_distancia']
+
+    while (Este_nodo != Inicio):
+        Este_nodo = matriz_distancias[Este_nodo]['predecesor']
+        distancia_Andreina += 2
+
+        if Este_nodo in Cadena_Javier and Este_nodo != nodoInicio:
+            print("\n--PROBLEMA DETECTADO--")
+            print("Andreina camino: " + str(Cadena_Andreina) + " Andreina quiere pasar por el nodo "+str(Este_nodo))
+            print("Y Javier ya pasa por el nodo " + str(Este_nodo) + "Buscando camino alterno:")
+            Este_nodo = Cadena_Andreina.pop()
+            print("Buscando camino alterno para el nodo (" + str(Este_nodo) +") ...")
+
+            while matriz_distancias[Este_nodo]['predecesor_alt'] == None:
+                distancia_Andreina -= 2
+                Este_nodo = Cadena_Andreina.pop()
+                print("Buscando camino alterno para el nodo (" + str(Este_nodo)+") ...")
+
+            Cadena_Andreina.append(Este_nodo)
+            Este_nodo = matriz_distancias[Este_nodo]['predecesor_alt']
+        Cadena_Andreina.append(Este_nodo)
+
+    print("\nReconstruyendo el camino más corto desde el nodo inicio ("+str(Inicio)+") hasta el nodo destino ("+str(DestinoJavier)+")")
+    print("CAMINO MÁS CORTO JAVIER: "+str(Cadena_Javier))
+    print("DISTANCIA: "+str(matriz_distancias[DestinoJavier]['min_distancia']))
+    distancia_Javier = matriz_distancias[DestinoJavier]['min_distancia']
+
+    print("CAMINO MÁS CORTO ANDREINA: "+str(Cadena_Andreina))
+    print("DISTANCIA: "+str(distancia_Andreina))
+
+    #info = Cadena_Javier + "/" + distancia_Javier + "/" + Cadena_Andreina + "/" + distancia_Andreina
+    string_javier = ""
+    string_andreina = ""
+
+    for i in range(len(Cadena_Javier)):
+        string_javier = string_javier + str(Cadena_Javier[i]) + "-"
+
+    for i in range(len(Cadena_Andreina)):
+        string_andreina = string_andreina + str(Cadena_Andreina[i]) + "-"
+
+    info = string_javier + "," + str(distancia_Javier) + "/" + string_andreina + "," + str(distancia_Andreina)
     
-    print("\nReconstruyendo el camino más corto desde el nodo inicio ("+str(Inicio)+") hasta el nodo destino ("+str(Destino)+")")
-    print("CAMINO MÁS CORTO: "+str(Cadena))
-    print("DISTANCIA: "+str(matriz_distancias[Destino]['min_distancia']))
+    f = open("temp.txt","w+")
+    f.write(info)
+    f.close()
+    
