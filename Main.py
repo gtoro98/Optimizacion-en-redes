c975b013c44346345d07c5662fe30aac3c7d8bb4
Carlos Freitas <47233287+CarlosSoki@users.noreply.github.com>
2021-11-21 20:04:36 -0400

    Update Main.py

--- Main.py
+++ Main.py
@@ -1,6 +1,6 @@
 #ELABORADO POR:
 #-Carlos Alejandro Freitas. CI: 25991527
-#-
+#-Miguel Colina CI: 23619726
 #-
 
 from matriz import *
@@ -8,4 +8,6 @@ from djikstra import *
 import gui
 
 gui.main_window_function()
+
+
 
